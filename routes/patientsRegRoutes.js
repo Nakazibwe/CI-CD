b2bbbca960c821052f7444d051519fe8e09c0031
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-17 12:09:47 +0300

    All routes were edited

--- routes/patientsRegRoutes.js
+++ routes/patientsRegRoutes.js
@@ -1,29 +1,17 @@
+/* eslint-disable no-throw-literal */
 /* eslint-disable no-console */
 const express = require('express');
-const mongoose = require('mongoose');
-const PatientsReg = require('../models/patientsRegModels');
+const patientController = require('../controllers/patientControllers');
 
 const router = express.Router();
 
 // Router to get the registration page.
-router.get('/', (req, res) => {
-  res.render('index');
-});
+router.get('/', patientController.getRegPage);
+
+// Get all the available patients.
+router.get('/patients', patientController.getPatients);
 
 // Router to post data from the form.
-router.post('/', async (req, res) => {
-  try {
-    const patientsReg = new PatientsReg(req.body);
-    await patientsReg.save((err) => {
-      if (err) {
-        throw err;
-      }
-      res.redirect('/');
-    });
-  } catch (error) {
-    console.log(error)
-    res.status(400).send('Data was not sent to Patients Database');
-  }
-});
+router.post('/', patientController.postPatients);
 
 module.exports = router;
