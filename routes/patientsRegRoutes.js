26ee9187a3c2125d6cb1ff2bb0ed834641f5db41
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-18 04:04:50 +0300

    Update and delete routes added to project

--- routes/patientsRegRoutes.js
+++ routes/patientsRegRoutes.js
@@ -14,4 +14,9 @@ router.get('/patients', patientController.getPatients);
 // Router to post data from the form.
 router.post('/', patientController.postPatients);
 
+// Route to update a patient.
+router.put('/patients/:id', patientController.updatePatient);
+
+// Route to delete a patient.
+router.delete('/patients/:id', patientController.deletePatient);
 module.exports = router;
